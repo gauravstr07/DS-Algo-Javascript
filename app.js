0958caec8d1b999ccd27a5dae4a75b25138ccf44
Gaurav Sutar <83762168+gauravstr07@users.noreply.github.com>
2022-10-13 16:57:38 +0530

    Sum Zero Problem

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('Calling app.js');
+//Checking sum zero - Problem--- 1
+// [-5, -4, -3, -2, 0, 2, 4, 6, 8]  -- Input
+// [?, ?] -> Output
+
+function getSumPairZero(array) {
+  for (let number of array) {
+    for (let j = 1; j < array.length; j++) {
+      if (number + array[j] === 0) {
+        return [number, array[j]];
+      }
+    }
+  }
+}
+
+const result = getSumPairZero([-5, -4, -3, -2, 0, 2, 4, 6, 8]);
+console.log(result);
